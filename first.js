0a5d66af0b75aa0b1c8dd956723035b80bee11d2
Prasad Arbad <prasadarbad9@gmail.com>
2024-04-10 07:00:23 +0530

    objects

--- first.js
+++ first.js
@@ -1,1 +1,8 @@
-console.log("Hello Javascript");
+const student ={
+    fullName:"Prasad Arbad",
+    age:20,
+    cgpa:8.2,
+    isPass:true
+}
+student["age"]=student["age"] + 1;
+ console.log(student["age"]);
